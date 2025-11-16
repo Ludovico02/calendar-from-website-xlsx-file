c6b3328c72d3bdc39966d898589769a329947feb
Ludovico Gardenal <ludovico02@outlook.com>
2025-11-16 16:30:39 +0100

    Implemented Calendar

--- src/api/fetchSchedule.js
+++
@@ -1,7 +0,0 @@
-export async function fetchSchedule(url) {
-   const response = await fetch(url);
-   if(!response.ok) throw new Error("Can't download XLSX file");
-
-   const blob = await response.blob();
-   return blob;
-}
---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,18 @@
+export default function Calendar() {
+    const [view, setView] = useState("week");
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => setView("week")}>Settimana</button>
+                <button onClick={() => setView("month")}>Mese</button>
+            </div>
+            
+            {view === "week" ? (
+                <WeekView />
+            ) : (
+                <MonthView />
+            )}
+        </div>
+    )
+}
