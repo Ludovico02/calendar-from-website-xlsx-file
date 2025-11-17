2c545438ba999571fce70600d5d554f0939eca1c
Ludovico Gardenal <ludovico02@outlook.com>
2025-11-17 15:09:10 +0100

    added date functionality alongside the week day h3 title

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -5,6 +5,7 @@ import WeekView from "./WeekView";
 export default function Calendar() {
     const [view, setView] = useState("week");
     const [data, setData] = useState([]);
+    const [viewCounter, setViewCounter] = useState(0); // 0 is the current week
 
     useEffect(() => {
         async function getData() {
@@ -19,15 +20,24 @@ export default function Calendar() {
         console.log(data);
     }, [data]);
 
+    useEffect(() => {
+        console.log(viewCounter);
+    }, [viewCounter]);
+
     return (
         <div>
             <div>
+                <button onClick={() => setViewCounter(viewCounter - 1)} disabled={viewCounter <= 0}>decrease</button>
+                <button onClick={() => setViewCounter(viewCounter + 1)} disabled={viewCounter >= 52}>increase</button>
+                <button onClick={() => setViewCounter(0)}>go to zero</button>
+            </div>
+            <div>
                 <button onClick={() => setView("week")}>Settimana</button>
-                <button onClick={() => setView("month")}>Mese</button>
+                {/* <button onClick={() => setView("month")}>Mese</button> */}
             </div>
             
             {view === "week" ? (
-                <WeekView data={data} />
+                <WeekView data={data} counter={viewCounter} />
             ) : (
                 <MonthView />
             )}
--- src/components/Calendar/WeekView.jsx
+++ src/components/Calendar/WeekView.jsx
@@ -1,23 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../../css/WeekView.css"
 
-export default function WeekView({ data }) {
+export default function WeekView({ data, counter }) {
     const days = ["Lun", "Mar", "Mer", "Gio", "Ven"];
+    const currentDate = new Date();
+    const [currentViewWeek, setCurrentViewWeek] = useState(Array(5).fill(0));
 
-    console.log("Data from Calendar: ", data);
-
-    const getMonday = (date) => {
+    // Finds first day of the week (monday) and shifts it according to the week we want to see
+    const getMonday = (date, shift) => {
         date = new Date(date);
         const day = date.getDay();
-        const diff = date.getDate() - day + (day === 0 ? -6 : 1);
+        const diff = date.getDate() - day + (day === 0 ? -6 : 1) + shift;
         return new Date(date.setDate(diff));
     }
 
+    const getCurrentViewWeek = (shift) => {
+        const firstDay = getMonday(currentDate, shift).getDate();
+        
+        setCurrentViewWeek(prev => (
+            prev.map((_, i) => firstDay + i)
+        ));
+
+        console.log(currentViewWeek);
+    }
+
+    useEffect(() => {
+        getCurrentViewWeek(counter * 7);
+    }, [counter])
+
     return (
         <div className="week-grid">
-            {days.map(day => (
+            {days.map((day, i) => (
                 <div key={day} className="day-column">
-                    <h3>{day}</h3>
+                    <h3>{`${day} ${currentViewWeek[i]}`}</h3>
                     <p>{data[1]?.teacher}</p>
                 </div>
             ))}
